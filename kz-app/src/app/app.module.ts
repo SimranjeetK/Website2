1b2527aaf6c2e566a2b22859e082271ffb9fa142
meth.jessepinkman <vishnureddy529@gmail.com>
2019-03-07 12:46:12 -0800

    Corrected Home and Navbars

--- kz-app/src/app/app.module.ts
+++ kz-app/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { SplashComponent } from './splash/splash.component';
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { PortraitComponent } from './portrait/portrait.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SplashComponent,
     HomeComponent,
-    NavbarComponent
+    NavbarComponent,
+    PortraitComponent
   ],
   imports: [
     BrowserModule
